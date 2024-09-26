982f60cb6a47440331ee1bf27fafaf9999e575bc
unknown <moisesjuniorcas20@gmail.com>
2024-09-26 18:31:44 -0500

    agregar dependencias

--- src/test/java/test/java/com/example/SeleniumTest.java
+++ src/test/java/test/java/com/example/SeleniumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Test; 
 
 public class SeleniumTest {
 
